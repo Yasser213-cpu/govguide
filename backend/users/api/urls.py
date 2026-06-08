3bf71ab5aa05a771debf10d728d72d22fb2e3fdc
mousa <mohamed.mouza469@gmail.com>
2026-06-08 12:40:35 +0300

    change refresh token url

--- backend/users/api/urls.py
+++ backend/users/api/urls.py
@@ -6,6 +6,6 @@ from . import views
 
 urlpatterns = [
     path("token",TokenObtainPairView.as_view()),
-    path("refresh",TokenRefreshView.as_view() ),
+    path("token/refresh",TokenRefreshView.as_view() ),
     path("register" ,  views.RegisterAPIView.as_view() )
 ]
