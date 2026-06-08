499e352859b0ed484bc4894e2c3521baccc21a7a
AhmedGaballah2 <agaballah223@gmail.com>
2026-06-08 21:59:38 +0300

    feat: add integrate sentry in react + django

--- frontend/GovGuideFrontend/src/main.jsx
+++ frontend/GovGuideFrontend/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import App from "./App.jsx";
+
+Sentry.init({
+  dsn: "YOUR_SENTRY_DSN",
+
+  // ده “رابط المشروع على Sentry dashboard”
+
+  tracesSampleRate: 1.0,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
